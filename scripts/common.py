a2ced999676e3f361050877d8c4955d4e3d668aa
jackyu <jackyu@yunify.com>
2020-08-03 21:00:13 +0800

    done with new kernel

--- scripts/common.py
+++ scripts/common.py
@@ -30,9 +30,9 @@ def get_role():
 
 def get_hostname():
     role = get_role()
-    if role is ROLE_COMPUTE:
+    if role == ROLE_COMPUTE:
         with open(CMP_SID_INFO, "r") as info:
             sid = info.read().strip()
-        return "{}{}".format(role, sid)
+        return "{}{}".format("node", sid)
     else:
         return role
